af58a9e85858669748c5d71d94b8b80fc229781e
adnanpalli <34090986+adnanpalli@users.noreply.github.com>
2019-11-15 18:14:52 +0300

    Update welcome.blade.php

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -14,7 +14,7 @@
       </div>
     </div>
     <div class="col-md-8">
-      
+      <!--NEW COMMENT ADDED-->
         @foreach($posts as $post)
         <div class="post">
           <h2>{{  $post->title }}</h2>
